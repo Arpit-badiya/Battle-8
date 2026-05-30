a76b9ab983cf5b410c22d808c8186612ff6171a9
Arpit-badiya <arpitpatidar2506@gmail.com>
2026-05-30 17:06:54 +0530

    new update

--- src/constants/download.ts
+++ src/constants/download.ts
@@ -1,3 +1,3 @@
 /** Google Drive direct APK download */
 export const APK_DOWNLOAD_URL =
-  'https://drive.google.com/uc?export=download&id=1rjvLKOzsMNGtB0IClolCYdihOcXqNVzI'
+  'https://drive.google.com/uc?export=download&id=1BjiLMdKtU_ngvGznVmxPxr9wMsaOX1bi'
